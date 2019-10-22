ab103de606ec5a8663450ea58aaa0acc772e6ccb
whywaita <whywaita@whywrite.it>
2019-10-22 15:14:29 +0900

    Implement GetConversationsList

--- convert.go
+++ convert.go
@@ -11,23 +11,97 @@ const (
 	ErrMethodNotSupportedForChannelType = "method_not_supported_for_channel_type"
 )
 
+func GetConversationsList(api *slack.Client, types []slackutilsx.ChannelType) ([]slack.Channel, error) {
+	var paramTypes []string
+
+	for _, t := range types {
+		switch t {
+		case slackutilsx.CTypeChannel:
+			paramTypes = append(paramTypes, "public_channel")
+		case slackutilsx.CTypeGroup:
+			paramTypes = append(paramTypes, "private_channel")
+		case slackutilsx.CTypeDM:
+			paramTypes = append(paramTypes, "im")
+		default:
+			paramTypes = append(paramTypes, "")
+		}
+	}
+
+	param := &slack.GetConversationsParameters{
+		ExcludeArchived: "true",
+		Types:           paramTypes,
+	}
+
+	channels, nextCursor, err := api.GetConversations(param)
+	if err != nil {
+		return nil, err
+	}
+
+	for nextCursor != "" {
+		param = &slack.GetConversationsParameters{
+			ExcludeArchived: "true",
+			Types:           paramTypes,
+			Cursor: nextCursor,
+		}
+		c, n, err := api.GetConversations(param)
+		if err != nil {
+			return nil, err
+		}
+		channels = Concat(channels, c)
+		nextCursor = n
+	}
+
+	return channels, nil
+}
+
+func Concat(xs, ys []slack.Channel) []slack.Channel {
+	zs := make([]slack.Channel, len(xs) + len(ys))
+
+	for i, x := range xs {
+		zs[i] = x
+	}
+
+	l := len(xs)
+	for j, y := range ys {
+		zs[l + j] = y
+	}
+
+	return zs
+}
+
+func ConvertDisplayPrivateChannel(api *slack.Client, channelId string) (string, error) {
+	channels, err := GetConversationsList(api, []slackutilsx.ChannelType{slackutilsx.CTypeGroup})
+	if err != nil {
+		return "", err
+	}
+
+	for _, c := range channels {
+		if c.ID == channelId {
+			return c.Name, nil
+		}
+	}
+
+	return "", errors.New("channel not found")
+}
+
 func ConvertDisplayChannelName(api *slack.Client, ev *slack.MessageEvent) (fromType, name string, err error) {
 	// identify channel or group (as known as private channel) or DM
 
 	channelType := slackutilsx.DetectChannelType(ev.Channel)
+	fromType = channelType.String()
 	switch channelType {
 	case slackutilsx.CTypeChannel:
-		fromType = "channel"
 		info, err := api.GetChannelInfo(ev.Channel)
 		if err != nil {
 			if err.Error() == ErrMethodNotSupportedForChannelType {
 				// This error occurred by the private channels only converted from the public channel.
 				// So, this is private channel if this error.
-				info, err := api.GetGroupInfo(ev.Channel)
+				name, err := ConvertDisplayPrivateChannel(api, ev.Channel)
 				if err != nil {
 					return "", "", err
 				}
-				return "group", info.Name, nil
+
+				return fromType, name, nil
 			} else {
 				return "", "", err
 			}
@@ -36,7 +110,6 @@ func ConvertDisplayChannelName(api *slack.Client, ev *slack.MessageEvent) (fromT
 		return fromType, info.Name, nil
 
 	case slackutilsx.CTypeGroup:
-		fromType = "group"
 		info, err := api.GetGroupInfo(ev.Channel)
 		if err != nil {
 			return "", "", err
@@ -48,7 +121,6 @@ func ConvertDisplayChannelName(api *slack.Client, ev *slack.MessageEvent) (fromT
 		if ev.Msg.SubType != "" {
 			// SubType is not define user
 		} else {
-			fromType = "DM"
 			info, err := api.GetUserInfo(ev.Msg.User)
 			if err != nil {
 				return "", "", err
@@ -57,7 +129,6 @@ func ConvertDisplayChannelName(api *slack.Client, ev *slack.MessageEvent) (fromT
 			return fromType, info.Name, nil
 		}
 	default:
-		fromType = ""
 		name = ""
 	}
 
@@ -101,6 +172,4 @@ func ConvertDisplayUserName(api *slack.Client, ev *slack.MessageEvent, id string
 
 		return byInfo.Name, "user", nil
 	}
-
-	return "", "", errors.New("user not found")
 }
