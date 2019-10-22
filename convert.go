158ce55392ffac4f96cc5729ddc4b01b1e2198f1
whywaita <whywaita@whywrite.it>
2019-10-22 12:07:21 +0900

    refactor and fix bug https://github.com/whywaita/aguri/issues/26

--- convert.go
+++ convert.go
@@ -1,61 +1,62 @@
 package slack_lib
 
 import (
-	"errors"
+	"github.com/pkg/errors"
 
 	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackutilsx"
+)
+
+var (
+	ErrMethodNotSupportedForChannelType = errors.New("method_not_supported_for_channel_type")
 )
 
 func ConvertDisplayChannelName(api *slack.Client, ev *slack.MessageEvent) (fromType, name string, err error) {
 	// identify channel or group (as known as private channel) or DM
 
-	// Channel prefix : C
-	// Group prefix : G
-	// Direct message prefix : D
-	for _, c := range ev.Channel {
-		if string(c) == "C" {
-			fromType = "channel"
-			info, err := api.GetChannelInfo(ev.Channel)
-
+	channelType := slackutilsx.DetectChannelType(ev.Channel)
+	switch channelType {
+	case slackutilsx.CTypeChannel:
+		fromType = "channel"
+		info, err := api.GetChannelInfo(ev.Channel)
+		if err == ErrMethodNotSupportedForChannelType {
+			// This error occurred by the private channels only converted from the public channel.
+			// So, this is private channel if this error.
+			info , err := api.GetGroupInfo(ev.Channel)
 			if err != nil {
 				return "", "", err
 			}
-
-			name = info.Name
-
-			return fromType, name, nil
-		} else if string(c) == "G" {
-			fromType = "group"
-			info, err := api.GetGroupInfo(ev.Channel)
-
-			if err != nil {
-				return "", "", err
-			}
-
-			name = info.Name
-
-			return fromType, name, nil
-		} else if string(c) == "D" {
-			if ev.Msg.SubType != "" {
-				// SubType is not define user
-			} else {
-				fromType = "DM"
-				info, err := api.GetUserInfo(ev.Msg.User)
-
-				if err != nil {
-					return "", "", err
-				}
-
-				name = info.Name
-
-				return fromType, name, nil
-			}
-		} else {
-			fromType = ""
-			name = ""
+			return "group", info.Name, nil
+		} else if err != nil {
+			return "", "", err
 		}
 
-		break
+		return fromType, info.Name, nil
+
+	case slackutilsx.CTypeGroup:
+		fromType = "group"
+		info, err := api.GetGroupInfo(ev.Channel)
+		if err != nil {
+			return "", "", err
+		}
+
+		return fromType, info.Name, nil
+
+	case slackutilsx.CTypeDM:
+		if ev.Msg.SubType != "" {
+			// SubType is not define user
+		} else {
+			fromType = "DM"
+			info, err := api.GetUserInfo(ev.Msg.User)
+			if err != nil {
+				return "", "", err
+			}
+
+			return fromType, info.Name, nil
+		}
+	default:
+		fromType = ""
+		name = ""
 	}
 
 	return "", "", errors.New("channel not found")
